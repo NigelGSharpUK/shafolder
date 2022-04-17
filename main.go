38f6487b6fe03b967b11fa6facc36cc221e8d1df
NigelGSharpUK <nigel@preferredsyntax.co.uk>
2022-04-17 08:36:31 +0100

    Report filesize of file specified on command line

--- main.go
+++ main.go
@@ -1,5 +1,19 @@
 package main
 
+import (
+	"fmt"
+	"os"
+)
+
 func main() {
-	println("Hello World")
+	if len(os.Args) != 2 {
+		fmt.Println("Please provide one filename")
+	} else {
+		fi, err := os.Stat(os.Args[1])
+		if err != nil {
+			fmt.Println(err.Error())
+		} else {
+			fmt.Println("Filesize of ", os.Args[1], ": ", fi.Size())
+		}
+	}
 }
