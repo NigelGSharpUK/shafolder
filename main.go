296bc8c711b662fae69f7ad0084099fb53e90018
NigelGSharpUK <nigel@preferredsyntax.co.uk>
2022-04-17 09:04:56 +0100

    Sha256 of the given file's contents

--- main.go
+++ main.go
@@ -1,19 +1,28 @@
 package main
 
 import (
+	"crypto/sha256"
 	"fmt"
+	"io"
+	"log"
 	"os"
 )
 
 func main() {
 	if len(os.Args) != 2 {
-		fmt.Println("Please provide one filename")
-	} else {
-		fi, err := os.Stat(os.Args[1])
-		if err != nil {
-			fmt.Println(err.Error())
-		} else {
-			fmt.Println("Filesize of ", os.Args[1], ": ", fi.Size())
-		}
+		log.Fatal("Please provide one filename")
 	}
+	f, err := os.Open(os.Args[1])
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+
+	h := sha256.New()
+	if _, err := io.Copy(h, f); err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Print("sha256 of the file " + os.Args[1] + ": ")
+	fmt.Printf("%x", h.Sum(nil))
 }
