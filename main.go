a0723f9a0d0ee20e5f079eba072dbe7f7b440c50
NigelGSharpUK <nigel@preferredsyntax.co.uk>
2022-04-17 09:22:56 +0100

    -terse command line option

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"crypto/sha256"
+	"flag"
 	"fmt"
 	"io"
 	"log"
@@ -9,20 +10,30 @@ import (
 )
 
 func main() {
-	if len(os.Args) != 2 {
+	// Deal with command line arguments
+	flagTerse := flag.Bool("terse", false, "Shows fewer helpful hints")
+	flag.Parse()
+	if len(flag.Args()) != 1 {
 		log.Fatal("Please provide one filename")
 	}
-	f, err := os.Open(os.Args[1])
+	flagFilename := flag.Args()[0]
+
+	// Open the file
+	f, err := os.Open(flagFilename)
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer f.Close()
 
+	// Calculate the sha256 of file contents
 	h := sha256.New()
 	if _, err := io.Copy(h, f); err != nil {
 		log.Fatal(err)
 	}
 
-	fmt.Print("sha256 of the file " + os.Args[1] + ": ")
+	// Output
+	if !*flagTerse {
+		fmt.Print("sha256 of the file " + flagFilename + ": ")
+	}
 	fmt.Printf("%x", h.Sum(nil))
 }
